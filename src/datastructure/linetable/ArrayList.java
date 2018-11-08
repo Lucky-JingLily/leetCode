8272cdb304a5eedb3a69b54e5b47050b9073387a
lipeijing <lipeijing@jd.com>
2018-11-08 21:14:32 +0800

    数据结构线性表

---
+++ src/datastructure/linetable/ArrayList.java
@@ -0,0 +1,68 @@
+package datastructure.linetable;
+
+/**
+ * 顺序表
+ * 底层采用的数组，但是长度可以动态的变化
+ */
+
+public class ArrayList implements List {
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public Object get(int i) {
+        return null;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public boolean contains(Object e) {
+        return false;
+    }
+
+    @Override
+    public int indexOf(Object e) {
+        return 0;
+    }
+
+    @Override
+    public void add(int i, Object e) {
+
+    }
+
+    @Override
+    public void add(Object e) {
+
+    }
+
+    @Override
+    public boolean addBefore(Object obj, Object e) {
+        return false;
+    }
+
+    @Override
+    public boolean addAfter(Object obj, Object e) {
+        return false;
+    }
+
+    @Override
+    public Object remove(int i) {
+        return null;
+    }
+
+    @Override
+    public boolean remove(Object e) {
+        return false;
+    }
+
+    @Override
+    public Object replace(int i, Object e) {
+        return null;
+    }
+}
---
+++ src/datastructure/linetable/TestArrayList.java
@@ -0,0 +1,8 @@
+package datastructure.linetable;
+
+public class TestArrayList {
+
+    public static void main(String[] args) {
+        ArrayList list;
+    }
+}
